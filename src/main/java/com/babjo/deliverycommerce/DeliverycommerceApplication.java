520d0741d9f7dbe41c9e8f7c595bc4cb7ebde980
DJP <straycat405@gmail.com>
2026-02-25 16:16:03 +0900

    init commit

---
+++ src/main/java/com/babjo/deliverycommerce/DeliverycommerceApplication.java
@@ -0,0 +1,13 @@
+package com.babjo.deliverycommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliverycommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliverycommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/babjo/deliverycommerce/DeliverycommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.babjo.deliverycommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliverycommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

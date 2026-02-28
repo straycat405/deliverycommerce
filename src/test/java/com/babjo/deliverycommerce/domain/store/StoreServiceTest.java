4f604131d567c8bd33ed3f6cd3585a1d08d19081
jihoon <git40@gmail.com>
2026-02-28 16:26:21 +0900

    test: StoreService 가게 수정 권한 예외 테스트 추가

---
+++ src/test/java/com/babjo/deliverycommerce/domain/store/StoreServiceTest.java
@@ -0,0 +1,78 @@
+package com.babjo.deliverycommerce.domain.store;
+
+import com.babjo.deliverycommerce.domain.store.dto.StoreUpdateRequestDto;
+import com.babjo.deliverycommerce.domain.store.entity.Store;
+import com.babjo.deliverycommerce.domain.store.repository.StoreRepository;
+import com.babjo.deliverycommerce.domain.store.service.StoreService;
+import com.babjo.deliverycommerce.global.exception.CustomException;
+import com.babjo.deliverycommerce.global.exception.ErrorCode;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+class StoreServiceTest {
+
+    @Mock
+    private StoreRepository storeRepository;
+
+    @InjectMocks
+    private StoreService storeService;
+
+    @Test
+    @DisplayName("존재하지 않는 가게를 조회하면 STORE_NOT_FOUND 예외가 발생한다")
+    void getStore_fail_whenStoreNotFound() {
+        // given
+        UUID storeId = UUID.randomUUID();
+
+        given(storeRepository.findByStoreIdAndDeletedAtIsNull(storeId))
+                .willReturn(Optional.empty());
+
+        // when & then
+        assertThatThrownBy(() -> storeService.get(storeId))
+                .isInstanceOf(CustomException.class)
+                .extracting("errorCode")
+                .isEqualTo(ErrorCode.STORE_NOT_FOUND);
+    }
+
+    @Test
+    @DisplayName("가게 주인이 아닌 사용자가 수정하면 STORE_FORBIDDEN 예외가 발생한다")
+    void update_fail_whenActorIsNotOwner() {
+
+        //given
+        UUID storeID = UUID.randomUUID();
+        Long ownerId = 1L;
+        Long actorUserId = 2L;
+
+        Store store = Store.create(
+                ownerId,
+                "KOREAN",
+                "밥집",
+                "서울시 강남구"
+        );
+
+        StoreUpdateRequestDto request = new StoreUpdateRequestDto(
+                "CHICKEN",
+                "치킨집",
+                "서울시 서초구"
+        );
+
+        given(storeRepository.findByStoreIdAndDeletedAtIsNull(storeID)).willReturn(Optional.of(store));
+
+        //when then
+        assertThatThrownBy(() -> storeService.update(storeID, actorUserId, request))
+                .isInstanceOf(CustomException.class)
+                .extracting("errorCode")
+                .isEqualTo(ErrorCode.STORE_FORBIDDEN);
+    }
+}

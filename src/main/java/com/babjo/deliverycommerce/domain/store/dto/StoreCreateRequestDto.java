bf599b212ff5130ec11040aaaba468f2dfd811d9
jihoon <git40@gmail.com>
2026-02-28 16:21:53 +0900

    refactor: Store 요청 DTO 생성자 추가

--- src/main/java/com/babjo/deliverycommerce/domain/store/dto/StoreCreateRequestDto.java
+++ src/main/java/com/babjo/deliverycommerce/domain/store/dto/StoreCreateRequestDto.java
@@ -2,9 +2,13 @@ package com.babjo.deliverycommerce.domain.store.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class StoreCreateRequestDto {
 
     @NotBlank
--- src/main/java/com/babjo/deliverycommerce/domain/store/dto/StoreUpdateRequestDto.java
+++ src/main/java/com/babjo/deliverycommerce/domain/store/dto/StoreUpdateRequestDto.java
@@ -1,10 +1,14 @@
 package com.babjo.deliverycommerce.domain.store.dto;
 
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class StoreUpdateRequestDto {
 
     @Size(max = 50)

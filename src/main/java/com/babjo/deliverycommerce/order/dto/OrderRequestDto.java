710ef6609834ff70a0e58fe50629d0ea48e501f6
kim-jinyeong <rlawlsdud51@naver.com>
2026-03-03 12:50:41 +0900

    feat : Order DTO 작성

---
+++ src/main/java/com/babjo/deliverycommerce/order/dto/OrderRequestDto.java
@@ -0,0 +1,44 @@
+package com.babjo.deliverycommerce.order.dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.UUID;
+
+public class OrderRequestDto {
+    @Getter
+    @NoArgsConstructor
+    public static class CreateOrder{
+        @NotNull(message = "가게 ID는 필수입니다.")
+        private UUID storeId;
+
+        @NotBlank(message = "배송지는 필수입니다.")
+        private String address;
+
+        private String message;
+
+        @NotEmpty(message = "주문 항목은 1개 이상이어야 합니다.")
+        private List<OrderItemRequest> orderItems;
+    }
+
+    @Getter
+    @NoArgsConstructor
+    public static class OrderItemRequest{
+        @NotNull(message = "상품 ID는 필수입니다.")
+        private UUID productId;
+
+        @Min(value = 1, message = "수량은 1개 이상이어야 합니다.")
+        private Integer orderCount;
+
+        @NotNull(message = "가격 정보가 없습니다.")
+        private Integer productPrice;
+
+
+    }
+
+}
---
+++ src/main/java/com/babjo/deliverycommerce/order/dto/OrderResponseDto.java
@@ -0,0 +1,40 @@
+package com.babjo.deliverycommerce.order.dto;
+
+import com.babjo.deliverycommerce.order.entity.OrderStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class OrderResponseDto {
+    private UUID orderId;
+    private Long userId;
+    private UUID storeId;
+    private OrderStatus status;
+    private Integer totalPrice;
+    private String address;
+    private String message;
+    private List<OrderItemResponse> orderItems;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime acceptedAt;
+    private LocalDateTime canceledAt;
+    private String cancelReason;
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    public static class OrderItemResponse{
+        private Long id;
+        private UUID productId;
+        private Integer orderPrice;
+        private Integer count;
+
+    }
+}

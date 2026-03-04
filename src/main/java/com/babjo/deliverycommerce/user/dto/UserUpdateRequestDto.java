ad253d738310f2da0b5167f6139d7da371e59f71
DongJin Park <straycat405@gmail.com>
2026-03-04 10:39:01 +0900

    feature - 사용자 수정 dto 추가

---
+++ src/main/java/com/babjo/deliverycommerce/user/dto/UserUpdateRequestDto.java
@@ -0,0 +1,45 @@
+package com.babjo.deliverycommerce.user.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserUpdateRequestDto {
+
+    @Pattern(
+            regexp = "^[a-z0-9]{4,10}$",
+            message = "아이디는 4자 이상 10자 이하여야 하며, 알파벳 소문자와 숫자만 사용 가능합니다."
+    )
+    private String username;
+
+    // 기존 비밀번호 - 수정을 위해선 반드시 필요
+    @NotBlank(message = "비밀번호는 필수 입력값입니다.")
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]).{8,15}$",
+            message = "비밀번호는 8자 이상 15자 이하여야 하며, 알파벳 대소문자와 숫자, 특수문자가 포함되어야 합니다."
+    )
+    private String prePw;
+
+    // 새로운 비밀번호
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]).{8,15}$",
+            message = "비밀번호는 8자 이상 15자 이하여야 하며, 알파벳 대소문자와 숫자, 특수문자가 포함되어야 합니다."
+    )
+    private String postPw;
+
+    // 새로운 비밀번호 확인
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]).{8,15}$",
+            message = "비밀번호는 8자 이상 15자 이하여야 하며, 알파벳 대소문자와 숫자, 특수문자가 포함되어야 합니다."
+    )
+    private String postPwConfirm;
+
+    @Email(message = "올바른 이메일 형식이 아닙니다.")
+    private String email;
+
+    private String nickname;
+}
---
+++ src/main/java/com/babjo/deliverycommerce/user/dto/UserUpdateResponseDto.java
@@ -0,0 +1,26 @@
+package com.babjo.deliverycommerce.user.dto;
+
+import com.babjo.deliverycommerce.user.entity.User;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class UserUpdateResponseDto {
+    private final Long userId;
+    private final String username;
+    private final String email;
+    private final String nickname;
+    private final String role;
+    private final LocalDateTime updatedAt;
+
+    // 엔티티 -> DTO 변환은 DTO 생성자 쪽에서 처리
+    public UserUpdateResponseDto(User user) {
+        this.userId = user.getUserId();
+        this.username = user.getUsername();
+        this.nickname = user.getNickname();
+        this.email = user.getEmail();
+        this.role = user.getRole().name();
+        this.updatedAt = user.getUpdatedAt();
+    }
+}

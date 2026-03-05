c4f3c90846a60cfc388bdf92da9b42657bd7de7f
Myeongwoo <143478944+Lazernes@users.noreply.github.com>
2026-03-05 15:31:46 +0900

    [feature] Product 도메인 구현
    * feat: Product 도메인 기본 CRUD 구현
    
    * feat: Product 도메인 hide, show 구현
    
    * feat: Ai 설명 자동완성 기능 추가
    
    * refactor: 오타 수정
    
    * feature: 권한별 접근 제어 설정
    
    * feature: Category 별 상품 조회 기능
    
    * feature: 예외처리 설정
    
    * feature: AI 설명 생성 실패 시 fallback 처리
    
    * Test: ProductService Test 코드 작성
    
    * Rename: Product 관련 directory를 domain directory로 이동

---
+++ src/main/java/com/babjo/deliverycommerce/domain/product/service/ProductService.java
@@ -0,0 +1,24 @@
+package com.babjo.deliverycommerce.domain.product.service;
+
+import com.babjo.deliverycommerce.global.security.UserPrincipal;
+import com.babjo.deliverycommerce.domain.product.dto.ProductCreateRequestDto;
+import com.babjo.deliverycommerce.domain.product.dto.ProductResponseDto;
+import com.babjo.deliverycommerce.domain.product.dto.ProductUpdateRequestDto;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface ProductService {
+    // Store 이후 연결
+    ProductResponseDto create(ProductCreateRequestDto request);
+    ProductResponseDto get(UUID productIdm,UserPrincipal user);
+    List<ProductResponseDto> getAll(UserPrincipal user);
+    List<ProductResponseDto> getByCategory(String category, UserPrincipal user);
+    ProductResponseDto update(UUID productId, ProductUpdateRequestDto request);
+    void delete(UUID productId, Long userId);
+
+    ProductResponseDto generateDescription(UUID productId, String point);
+
+    void hide(UUID productId);
+    void show(UUID productId);
+}

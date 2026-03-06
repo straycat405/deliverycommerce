93b194da023586728069b7881c999780e6932fe0
kim-jinyeong <rlawlsdud51@naver.com>
2026-03-06 10:59:01 +0900

    feature : N+1 문제 해결

--- src/main/java/com/babjo/deliverycommerce/domain/order/repository/OrderRepository.java
+++ src/main/java/com/babjo/deliverycommerce/domain/order/repository/OrderRepository.java
@@ -3,6 +3,7 @@ package com.babjo.deliverycommerce.domain.order.repository;
 import com.babjo.deliverycommerce.domain.order.entity.Order;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -10,10 +11,8 @@ import java.util.UUID;
 
 public interface OrderRepository extends JpaRepository<Order, UUID> {
 
-    // 삭제되지 않은 주문 조회
-    Optional<Order> findByIdAndDeletedAtIsNull(UUID orderId);
-
     // 사용자의 주문 내역 페이징 조회 ( 삭제한 내역 숨김 )
+    @EntityGraph(attributePaths = {"orderItems"})
     Page<Order> findByUserIdAndDeletedAtIsNull(Long userId, Pageable pageable);
 
 

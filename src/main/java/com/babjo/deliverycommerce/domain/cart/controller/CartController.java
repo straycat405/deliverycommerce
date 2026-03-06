775f2d80a9521896bc11f3d48582f4fe6e23221e
jihoon <git40@gmail.com>
2026-03-06 10:21:32 +0900

    feat: CartController 삭제 API 추가

--- src/main/java/com/babjo/deliverycommerce/domain/cart/controller/CartController.java
+++ src/main/java/com/babjo/deliverycommerce/domain/cart/controller/CartController.java
@@ -7,6 +7,7 @@ import com.babjo.deliverycommerce.domain.cart.service.CartService;
 import com.babjo.deliverycommerce.global.security.CurrentUserResolver;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
@@ -52,5 +53,20 @@ public class CartController {
         log.info("장바구니 수량 수정 API 완료: userId={}, cartId={}, itemCount={}", userId, response.getCartId(), response.getItems().size());
 
         return response;
+    }
+
+    @DeleteMapping("/items/{cartItemId}")
+    public ResponseEntity<Void> deleteItem(@PathVariable UUID cartItemId,
+                                           Authentication authentication) {
+
+        Long userId = currentUserResolver.getUserId(authentication);
+
+        log.info("장바구니 항목 삭제 API 요청: userId={}, cartItemId={}", userId, cartItemId);
+
+        cartService.deleteItem(userId, cartItemId);
+
+        log.info("장바구니 항목 삭제 API 완료: userId={}, cartItemId={}", userId, cartItemId);
+
+        return ResponseEntity.noContent().build();
     }
 }
